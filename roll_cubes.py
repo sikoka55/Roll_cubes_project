93e48ea675e02ccb36914d5d874971342b44d355
Dany <kunkadan@gmail.com>
2022-08-30 00:39:45 +0300

    Update roll_cubes.py

--- roll_cubes.py
+++ roll_cubes.py
@@ -1,53 +1,31 @@
 import random
 
 
-def prnt_answer(cube: int):
+def prnt_answer(answer: int):
     print('Ось твій результат:')
-    print('*' * 9)
-    print(random.randint(1, cube))
-    print('*' * 9)
-
-
-
-def roll(answer):
-    if answer == 'd4':
-        prnt_answer(4)
-
-    elif answer == 'd6':
-        prnt_answer(6)
-
-    elif answer == 'd8':
-        prnt_answer(8)
-
-    elif answer == 'd10':
-        prnt_answer(10)
-
-    elif answer == 'd12':
-        prnt_answer(12)
-
-    elif answer == 'd20':
-        prnt_answer(20)
-
-    elif answer == 'd100':
-        prnt_answer(100)
+    print('♥' * 9)
+    print(' '*3, random.randint(1, answer))
+    print('♥' * 9)
 
 
 print('Привіт,мандрівник!')
 print('Я бачу, у тебе є потреба у роллі кубика')
+print()
 print('Який кубик тобі потрібно кинути?')
 print()
-print('d4,d6,d8,d10,d12,d20,d100 - обери свій и введи назву')
+print('к4,к6,к8,к10,к12,к20,к100 - обери свій и введи назву ')
 print()
 
 
 while True:
     answer = input()
-    if answer not in ['d4', 'd6', 'd8', 'd10', 'd12', 'd20', 'd100']:
+    if answer not in ['к4', 'к6', 'к8', 'к10', 'к12', 'к20', 'к100']:
         print('Обери кубик! Годі колупатися у носі!')
 
     else:
         print('Оце так! Ти вибрав', answer)
-        roll(answer)
+        answer = int(answer[1:])
+        prnt_answer(answer)
 
         print()
         print('Хочеш зробити ще один бросок? Напиши "так" або "ні"')
